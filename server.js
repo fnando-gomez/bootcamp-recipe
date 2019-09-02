5a26ddb439a1ebb513cbe05dbc42d24069d809b0
fnando <fnando.gomez@gmail.com>
2019-09-02 12:05:21 -0500

    without lights

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const server = express()
+const request = require ('request')
+const path = require ('path')
 
 const port = 8080
 
-server.get('/sanity',function(req,resp){
-    resp.send("Oki_doki")
+server.get('/sanity',function(req,res){
+    res.send("Oki_doki")
+})
+
+
+//Accesing the API
+server.get ('/recipes/:ingredient',(req,res) => {
+    request('https://recipes-goodness.herokuapp.com/recipes/YOUR_INGREDIENT', function (error, response, body){
+        let reqData = JSON.parse(body)
+        let recipeData = req.params
+        console.log(`Recipe:${recipeData.ingredient}`)
+
+        console.log(reqData)
+
+
+        res.send("testing")
+    })
+
+
+
+
 })
 
 
