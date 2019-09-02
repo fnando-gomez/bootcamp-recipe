b0094bd61586e3ed2342a2f86883e167b67d68fe
fnando <fnando.gomez@gmail.com>
2019-09-02 11:03:17 -0500

    server listening

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+const express = require('express')
+const server = express()
 
-console.log("This is working")
+const port = 8080
+
+server.get('/sanity',function(req,resp){
+    resp.send("Oki_doki")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(port, function(){
+    console.log(`Running on port ${port}`)
+})
 
