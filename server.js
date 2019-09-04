d4b3fcb331ac2b7579341be8e26093dfbe3e9162
fnando <fnando.gomez@gmail.com>
2019-09-04 00:14:49 -0500

    Receiving realData

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require('express')
+
+const express = require ('express')
 const server = express()
 const request = require ('request')
 const path = require ('path')
 
 const port = 8080
-
-server.get('/sanity',function(req,res){
-    res.send("Oki_doki")
-})
+server.get('/sanity', (req, res) => res.send("I'm alive and running"))
 
 
-//Accesing the API
-server.get ('/recipes/:ingredient',(req,res) => {
-    request(`https://recipes-goodness.herokuapp.com/recipes/${req.params}`, function (error, response, body){
-        let data = JSON.parse(body)
-        let recipeData = req.params
-        console.log(`Ingredient:${recipeData.ingredient}`)
 
-        
-        
+// Routes and data
+// API --> https://recipes-goodness.herokuapp.com/recipes/YOUR_INGREDIENT
 
-
-       res.send(data)
+server.get('/recipes/:ingredient', (req, res) => {
+    const ingredient = req.params.ingredient
+    let apiAddr =`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`
+        request(apiAddr, function(error, response, body){
+            let recipeData = JSON.parse(body).results//<-- Se agrega ".results" porque la API entrega: {results: [RECIPES]}, así se obtiene el array final
+            let realData = recipeData.map(r =>{
+                return{
+                    title: r.title,
+                    ingredients: r.ingredients,
+                    thumbnail: r.thumbnail,
+                    href: r.href, 
+                }
+            })            
+            res.send(realData)
+        })
     })
 
 
@@ -29,7 +34,8 @@ server.get ('/recipes/:ingredient',(req,res) => {
 
 
 
-})
+
+server.listen(port, () => console.log(`Server running on http://localhost:${port}`))
 
 
 
@@ -45,7 +51,91 @@ server.get ('/recipes/:ingredient',(req,res) => {
 
 
 
-server.listen(port, function(){
-    console.log(`Running on port ${port}`)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const server = express()
+// const request = require ('request')
+// const path = require ('path')
+
+// const port = 8080
+
+
+// server.get('/sanity',function(req,res){
+//     res.send("Oki_doki")
+// })
+
+// //Serving the files
+
+
+
+// //Accesing the API
+
+
+// server.get('/recipes/:ingredient', (req, res) => {
+
+
+
+//   res.send('GET request to the homepage')
+// })
+
+
+// // request(`https://recipes-goodness.herokuapp.com/recipes/${req.params}`, function (error, response, body){
+// //     let data = JSON.parse(body)
+// //     let recipeData = req.params
+// //     console.log(`Ingredient:${recipeData.ingredient}`)
+
+
+
+
+
+
+
+
+
+
+
+
+// server.listen(port, function(){
+//     console.log(`Running on http://localhost:${port}`)
+// })
+
+
+
+
+
 
